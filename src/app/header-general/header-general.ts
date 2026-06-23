6d43772e0095a5580a0355622393af9dfc80d40e
Agustin Surkan <agustinsurkan@gmail.com>
2026-06-22 22:56:39 -0300

    feat: header component

--- src/app/header-general/header-general.ts
+++ src/app/header-general/header-general.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
 
 @Component({
   selector: 'app-header-general',
   imports: [],
+  standalone: true,
   templateUrl: './header-general.html',
   styleUrl: './header-general.css',
 })
-export class HeaderGeneral {}
+export class HeaderGeneral {
+  currentView = input.required<string>();
+  navigate = output<string>();
+  menuOpen = signal(false);
+
+  toggleMenu() {
+    this.menuOpen.update((v) => !v);
+  }
+
+  onNavigate(view: string) {
+    this.navigate.emit(view);
+  }
+}
