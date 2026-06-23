4c708107bad559f6ecc2259b6df45acaa4905cc1
Agustin Surkan <agustinsurkan@gmail.com>
2026-06-22 22:28:34 -0300

    WIP: Header Base

---
+++ src/app/header-general/header-general.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-general',
+  imports: [],
+  templateUrl: './header-general.html',
+  styleUrl: './header-general.css',
+})
+export class HeaderGeneral {}
