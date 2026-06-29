31f72528230e674ce7638858614be5c210538cac
Agustin Surkan <agustinsurkan@gmail.com>
2026-06-29 04:25:59 -0300

    feat: map component

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './general/app-footer/app-footer';
 import { HeaderGeneral } from './header-general/header-general';
 import { Banner } from './banner/banner';
+import { Maps } from './maps/maps';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderGeneral, FooterComponent, Banner],
+  imports: [RouterOutlet, HeaderGeneral, FooterComponent, Banner, Maps],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
---
+++ src/app/maps/maps.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-maps',
+  standalone: true,
+  imports: [],
+  templateUrl: './maps.html',
+  styleUrl: './maps.css',
+})
+export class Maps {}
