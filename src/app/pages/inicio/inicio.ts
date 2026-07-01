e0ecbdd63bff8e4a3ad0397c409905cdafb48522
Agustin Surkan <agustinsurkan@gmail.com>
2026-06-30 23:07:11 -0300

    feat: maps component

--- src/app/pages/inicio/inicio.ts
+++ src/app/pages/inicio/inicio.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Banner } from '../../components/banner/banner';
 import { CareerCarrousel } from '../../components/career-carousel/career-carousel';
+import { Maps } from '../../components/maps/maps';
 
 @Component({
   selector: 'app-inicio',
-  imports: [Banner, CareerCarrousel],
+  imports: [Banner, CareerCarrousel, Maps],
   templateUrl: './inicio.html',
   styleUrl: './inicio.css',
 })
